5de3a069b291c62a3f919f7ac439e782eb4e73e9
Kaik Esmil <159018517+Esmil2007@users.noreply.github.com>
2024-02-05 09:57:04 -0300

    Add files via upload

---
+++ frase.sh
@@ -0,0 +1,14 @@
+
+BIN/BASH
+echo "vamos iniciar as atividades"
+sudo apt update;
+sudo apt upgrade;
+echo"vamos instalar o apache"
+sudo apt install mysql-server;
+sudo apt install php;
+sudo apt install phpmyadmin;
+sudo apt install apache2;
+# escolha usar  APACHE e configure o phpmyadmin com usuario e senha para banco de dados
+#apoś isso abra o browser e teste : http://localhost/phpmyadmin 
+
+
