85c69025d5837ab467658f8b8c84c06ab1ec3e2e
Kaik Esmil <159018517+Esmil2007@users.noreply.github.com>
2024-02-28 10:31:15 -0300

    Update frase.sh

--- frase.sh
+++ frase.sh
@@ -8,6 +8,7 @@ sudo apt install mysql-server;
 sudo apt install php;
 sudo apt install phpmyadmin;
 sudo apt install apache2;
+sudo apt install ssh;
 # escolha usar  APACHE e configure o phpmyadmin com usuario e senha para banco de dados
 #apoś isso abra o browser e teste : http://localhost/phpmyadmin 
 
